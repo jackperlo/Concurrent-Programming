916c71af7685637a8d343cd767f5bcc802f665cc
Ale <a.mana.9883@vallauri.edu>
2020-12-01 08:16:38 -0800

    Ale: TIMENSEC

--- Master.c
+++ Master.c
@@ -24,8 +24,8 @@ void print_map(int isTerminal); /* stampa una vista della mappa durante l'esecuz
 const int SO_TAXI; /* numero di taxi presenti nella sessione in esecuzione */
 const int SO_CAP_MIN; /* capacità minima assumibile da SO_CAP: determina il minimo numero che può assumere il valore che identifica il massimo numero di taxi che possono trovarsi in una cella contemporaneamente */
 const int SO_CAP_MAX; /* capacità massima assumibile da SO_CAP: determina il MASSIMO numero che può assumere il valore che identifica il massimo numero di taxi che possono trovarsi in una cella contemporaneamente */
-const int SO_TIMESEC_MIN; /* valore minimo assumibile da SO_TIMESEC, che rappresenta il tempo di attraversamento di una cella della matrice */
-const int SO_TIMESEC_MAX; /* valore MASSIMO assumibile da SO_TIMESEC, che rappresenta il tempo di attraversamento di una cella della matrice */
+const int SO_TIMENSEC_MIN; /* valore minimo assumibile da SO_TIMESEC, che rappresenta il tempo di attraversamento di una cella della matrice */
+const int SO_TIMENSEC_MAX; /* valore MASSIMO assumibile da SO_TIMESEC, che rappresenta il tempo di attraversamento di una cella della matrice */
 
 /*-------------VARIABILI GLOBALI-------------*/
 int map[SO_HEIGHT][SO_WIDTH]; /* matrice che determina la mappa in esecuzione */
